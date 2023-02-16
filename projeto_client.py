a0d0ed01374599104f4aad851ca07ce893b85862
PedroLacerda22 <pedrogmlacerda@gmail.com>
2023-02-16 14:34:41 -0300

    Inicio do Projeto

---
+++ projeto_client.py
@@ -0,0 +1,12 @@
+import random 
+
+# definindo comandos (Byte array)
+c1 = bytearray(["00 00 00 00"])
+c2 = bytearray(["00 00 AA 00"])
+c3 = bytearray(["AA 00 00"])
+c4 = bytearray(["00 AA 00"])
+c5 = bytearray(["00 00 AA"])
+c6 = bytearray(["00 AA"])
+c7 = bytearray(["AA 00"])
+c8 = bytearray(["00"])
+c9 = bytearray(["FF"])
