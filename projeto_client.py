df4a93eec0bb47753e7a2834c09b68299e4eb10a
PedroLacerda22 <pedrogmlacerda@gmail.com>
2023-02-17 17:34:24 -0300

    Teoricamente a lógica está certa. Falta testar.

--- projeto_client.py
+++ projeto_client.py
@@ -10,6 +10,7 @@ def main():
     try:
         print("Iniciou o main")
         com1 = enlance(serialName)
+
         # Definindo comandos (Byte array)
         c1 = b'\x00\x00\x00\x00'
         c2 = b'\x00\x00\xAA\x00'
@@ -27,17 +28,61 @@ def main():
 
         # Criando os comandos de instrucao
         # byte inicial
-        c_init = b'\x05'  # sendo que o \x05 vai representar o inicio de uma serie de comandos novos.
+        m_init = b'\x05'  # \x05 representa o inicio da sequencia de comandos
+        # Vai ter um byte entre cada comando que diz o tamanho do próximo comando
+        m_end = b'\x06'   # \x06 representa o fim da sequencia de comandos 
+
         # comando de começo de transmissão, comando de começo de comando, comando de fim de comando, comando de fim de transmissão.
         # como verificar se nada foi perdido?
 
+        # Definindo o reebidor rxBuffer
+                
+        com1.enable()
+        txLen = len(txBuffer)
+        rxBuffer, nRx = com1.getData(txLen)
+
         num_com = random.randint(10, 30) # define um numero aleatorio de comandos a serem executados 
-        com1
+        
+        com1.sendData(np.asarray(m_init))
         for i in range (1, num_com):
+            # # com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
+            # com1.sendData(np.asarray(c_init))
+            # com1.sendData(np.asarray(comandos[random.randint(0,8)])) 
+            com_env = comandos[random.randint(0,8)]
+            str_com = str(com_env)
+            size = 0 # prone to bugs
+            for char in str_com:
+                if char == "x":
+                    size += 1
             
-            # com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
-            com1.sendData(np.asarray(comandos[random.randint(0,8)]))
-            
+            if size == 1:
+                com1.sendData(np.asarray(b'\x01')) # o próximo comando tem tamanho 1
+            if size == 2:
+                com1.sendData(np.asarray(b'\x02')) # o próximo comando tem tamanho 2
+            if size == 3:
+                com1.sendData(np.asarray(b'\x03')) # o próximo comando tem tamanho 3
+            if size == 4:
+                com1.sendData(np.asarray(b'\x04')) # o próximo comando tem tamanho 4
+            else:
+                print ("Deu erro no loop de size")
+                break
+
+            com1.sendData(np.asarray(com_env)) # comando sendo enviado
+            # Após o c_end, o proximo loop só pode iniciar se o servidor confirmar que a mensagem foi recebida. 
+
+            if com1.getData(txLen) != 1: # o servidor não confirmou que recebeu uma mensagem:
+                print("Servidor não confirmou que recebeu a mensagem")
+                break            
+
+        com1.sendData(np.asarray(m_end))
+ 
+        for i in range(5,0,-1): 
+            com1.getData(txLen)
+            if rxBuffer == '\x1B':
+                print("Sucesso!")
+                break
+            else:
+                time.sleep(1)
 
 
     except Exception as erro:
