a98910e60dfeebb210b5110f2499af9ab1536026
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2023-02-24 16:36:06 -0300

    Server

--- Server.py
+++ Server.py
@@ -1,20 +1,29 @@
-
 from tracemalloc import stop
+
+
 from enlace import *
 import time
 import numpy as np
+
 import time
 
-serialName = "COM8"           
+
+serialName = "COM8"                    # Windows(variacao de)
 
 def main():
     try:
+        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
+        #para declarar esse objeto é o nome da porta.
         com1 = enlace(serialName)
+        
+    
+        # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
 
         print("esperando 1 byte de sacrifício")        
         rxBuffer, nRx = com1.getData(1)
         com1.rx.clearBuffer()
+        time.sleep(.1)
         #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
         print("COMUNICAÇÃO ABERTA COM SUCESSO\n")
        
@@ -25,7 +34,6 @@ def main():
         nCmdInt = int.from_bytes(nCmd, "big")
         print(f"Serão recebidos {nCmdInt} pacotes de comandos\n")
         
-        lista = []
         x = 0
         while x < nCmdInt:
             # Recebendo o tamanho do pacote
@@ -36,24 +44,28 @@ def main():
 
             # Recebendo o pacote
             rxBuffer, rxBufferLen = com1.getData(rxBufferResponse)
-            lista.append(rxBuffer)
+
             x += 1
 
         print("Pacotes recebidos!\n")
 
         # Retornando quantidade de pacotes recebidos para o client
         print("Enviando a quantidade de pacotes recebidos ao client para confirmação\n")
-        com1.sendData(x.to_bytes(2, byteorder="big"))
-   
+        com1.sendData(nCmd)
 
+
+
+        print("-------------------------")
         print("Comunicação encerrada")
-      
+        print("-------------------------")
         com1.disable()
         
     except Exception as erro:
         print("ops! :-\\")
         print(erro)
         com1.disable()
+        
 
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
 if __name__ == "__main__":
     main()
